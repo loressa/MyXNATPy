dae9c7fadbb3dbed93a8e88e2c728839d408dc4e
loressa <les44@cam.ac.uk>
2020-06-16 17:25:15 +0100

    ADD: script to remove all subjects in a given project

---
+++ remove_subjects.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Script written by Lorena Escudero (Department of Radiology, University of Cambridge) 
+# to remove all subjects in an XNAT project - June 2020 
+#
+# Usage: edit xnathost/project/user and run 'python3 remove_subjects.py'
+# Note: This removes all subjects, can be modified to remove only some from an input list 
+# Note: User needs to have admin privileges in project for this script to work  
+#
+
+import xnat
+import os
+import getpass
+
+def remove_subjects(session, project_id):
+    xnat_project = session.projects[project_id]
+    # We find all subjects:
+    subjects = project.subjects
+    for subject in subjects:
+        subject.delete()
+    
+# ---------------------------------------------------------------------------------------------------------------------   
+
+if __name__ == '__main__':
+
+    xnathost   = 'URL'
+    project_id = 'NEW_PROJECT'
+    
+    user_id    = 'USER'
+    pwd        = getpass.getpass("Password for user name : %s = " % user_id)
+    
+    #input_list = '/path/to/input_list'
+
+    with xnat.connect(xnathost, user=user_id, password=pwd) as session:
+        remove_subjects(session, project_id)
+
